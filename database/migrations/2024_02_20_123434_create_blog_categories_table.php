f1fe869e49f3b76b8c73872ad092abfd63423cce
Zeyad Nada <zeyadn355@gmail.com>
2024-02-26 21:00:38 +0200

    add package-lock.json

--- database/migrations/2024_02_20_123434_create_blog_categories_table.php
+++ database/migrations/2024_02_20_123434_create_blog_categories_table.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blogs', function (Blueprint $table) {
+        Schema::create('blog_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('photo');
-            $table->string('content');
-            $table->string('reference');
+            $table->string('name');
+            // $table->string('description');
             $table->timestamps();
         });
     }
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('blogs');
+        Schema::dropIfExists('blog_categories');
     }
 };
---
+++ database/migrations/2024_02_21_085314_create_blogs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('thumbnail')->nullable();
+            $table->text('content');
+            $table->foreignId('category_id');
+            $table->foreign('category_id')->references('id')->on('blog_categories')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
